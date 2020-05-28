601909e4848a1a2319cb6d79b00a62ee99aa5a6c
jason.canto <rofsdg@gmail.com>
2020-05-27 21:13:51 -0300

    First commit

---
+++ src/main/java/canto/jason/DeathByAThousandTestsApplication.java
@@ -0,0 +1,13 @@
+package canto.jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeathByAThousandTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeathByAThousandTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/canto/jason/DeathByAThousandTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package canto.jason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeathByAThousandTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
